1406dc79fbfa026171907f74f466fdc1eb9ce5dd
NY <62198538+NYzyanai@users.noreply.github.com>
2021-01-27 23:41:57 +0900

    git clone git://github.com/jquery/sizzle.git

--- dom.js
+++ dom.js
@@ -1,32 +1,34 @@
 
-
-/*domモジュールってなに？DOM内の要素を探すのを非常に楽にする。DOMをさがすのに役立つヘルパー的関数を提供する。*/
-/*そもそもsizzleってなんなん？jqueryのなかの要素らしい*/
-
-
 round_phu.dom=(function(){
     var $ Sizzle;
-    
+
     function hasClass(el,clsName){
         var regex = new RegExp("(^|\\s)" + clsName + "(\\s|$)");
-        return regux.test(el.className);
+        return regex.test(el.className);
     }
 
     function addClass(el,clsName){
-        if(!hasClass(el,clsName)){
-            el.className+=" "+ clsName;
+        if (!hasClass(el,clsName)){
+            el.className +=" "+ clsName;
         }
     }
 
     function removeClass(el,clsName){
         var regex = new RegExp("(^|\\s)" + clsName + "(\\s|$)");
-        el.className=el.className.replace(regex," ");
+        el.className = el.className.replace(regex," ");
     }
+
+
+
+/*domモジュールってなに？DOM内の要素を探すのを非常に楽にする。DOMをさがすのに役立つヘルパー的関数を提供する。*/
+/*そもそもsizzleってなんなん？jqueryのなかの要素らしい*/
+
+
 
     return{
         $ : $,
-        hasClass:hasClass
-        addClass:addClass
-        removeClass:removeClass
+        hasClass : hasClass,
+        addClass : addClass,
+        removeClass : removeClass
     };
 })();
