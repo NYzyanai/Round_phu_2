9812ea184f709fcfa9dd2f073bf87ed9803d7e3a
NY <62198538+NYzyanai@users.noreply.github.com>
2021-01-30 19:21:25 +0900

    u

--- scripts/splash-screen.js
+++ scripts/splash-screen.js
@@ -1,3 +1,11 @@
+var height;
+var width;
+var screen_height;
+var screen_width;
+
+height=90;
+width=160;
+
 function imageDraw(){
     var cvs = document.getElementById('Round_phu');
     var ctx=cvs.getContext('2d');
@@ -7,6 +15,18 @@ function imageDraw(){
     splash_img.src= "src/fullscreen/round_phu.png";
 
     splash_img.onload = function(){
-        ctx.drawImage(splash_img,0,0,1600,900);
+        sizing();
+        ctx.imageSmoothingEnabled=false;
+        ctx.drawImage(splash_img,0,0,screen_width,screen_height);
     }
+}
+
+function sizing(){
+	screen_width = document.getElementById("wrapper").clientWidth;
+	screen_height = screen_width * (height / width);
+	var c = document.getElementById("Round_phu");
+	c.width = screen_width;
+	c.height = screen_height;
+	dw = screen_width / width;
+	dh = screen_height / height;
 }
