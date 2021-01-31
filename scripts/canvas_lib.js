0594e39c8e13d3d915ed51204a8bf12f938f4336
NY <62198538+NYzyanai@users.noreply.github.com>
2021-01-31 15:40:41 +0900

    Update canvas_lib.js
    height_match

--- scripts/canvas_lib.js
+++ scripts/canvas_lib.js
@@ -12,33 +12,31 @@ function sizing(){
     screen_width=document.getElementById("wrapper").clientWidth;
     screen_height=document.getElementById("wrapper").clientHeight;
 
-    var rate_screen;
-    rate_screen =screen_height/screen_width;
-
-    var static_rate;
-    static_rate=16/9;
-    /*console.log(static_rate)*/
-
     var canvas =document.getElementById("canvas");
-    console.log (rate_screen);
 
 
-    if (rate_screen>=static_rate){
-        //もしも縦長だった場合には、widthをscreenにあわせる
+    if (screen_width*9/16<screen_height){
+            //幅に合わせる
         canvas.width=screen_width;
         canvas.height=screen_width*9/16;
-        console.log("sita");
+        console.log("habaawase");
         
         canvas.style.left = (screen_width-canvas.width)/2+"px";
         canvas.style.top = (screen_height-canvas.height)/2+"px";
-    }else{
+    }else if(screen_width*9/16==screen_height){
         //もしも横ながだった場合には、heightをscreenに合わせる必要がある
         canvas.height=screen_height;
-        canvas.width=screen_height*static_rate;
-        console.log("ue");
-        canvas.style.top = (screen_height-canvas.height)/2+"px";
+        canvas.width=screen_width;
+        console.log("onazi");
+    }else if(screen_width*9/16>screen_height){
+        //高さにあわせる
+        canvas.height=screen_height;
+        canvas.width=screen_height*16/9;
+        console.log("takasaawase");
+
         canvas.style.left = (screen_width-canvas.width)/2+"px";
-    }
+        canvas.style.top = (screen_height-canvas.height)/2+"px";
+    }  
 
     
 
