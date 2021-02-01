c6999e090fec268ec156f0030873635404bea055
NY <62198538+NYzyanai@users.noreply.github.com>
2021-02-01 23:04:14 +0900

    dekita

--- scripts/main.js
+++ scripts/main.js
@@ -4,26 +4,65 @@ window.onload =function(){
     setInterval(loop,30);
 }
 
+var opacity;
 
-
+var start_flag;
 
 function loop(){
     //1秒間に30回よばれる
-
-    //したい判定
-    if (touchX<=160 && touchY>=40 ){
-        console.log("start");
-        //実効するプログラム
-        console.log(splash_img.style.opacity);
-        //ctx.fillrect=(0,0,canvas.width,canvas.height);
-        ctx.clearRect(0, 0, canvas.width, canvas.height);
-        // create the background gradient once
-
-    }else{
-        console.log(touchX + "と" + touchY);
+    if(opacity==null){
+        opacity=0;
     }
 
 
+        //したい判定
+        if (touchX<=160 && touchY>=40 ){
+            start_flag=1;
+            console.log("start");
+            //実効するプログラム
+            console.log(splash_img.style.opacity);
+            //ctx.fillrect=(0,0,canvas.width,canvas.height);
+            //ctx.clearRect(0, 0, canvas.width, canvas.height);
+            // create the background gradient once
+
+            //fade_in();
+
+            
+            
+
+
+
+        }else{
+            console.log(touchX + "と" + touchY);
+        }
     touchX=0;
     touchY=0;
+
+        if (start_flag==1){
+            fade_out();
+        }
+
+    //if (opacity<=1){
+        function fade_out(){
+            opacity =opacity +0.03;
+            console.log(opacity);
+            ctx.globalAlpha=opacity;
+            ctx.fillStyle="blaxk";
+            ctx.fillRect(0,0,canvas.width,canvas.height);
+
+            //}else{
+             //   console.log("finished clear");
+            //} 
+             }
+}
+
+
+function fade_in(){
+    let opacity= 0;
+    do {
+    opacity += 0.0001;
+    console.log(opacity);
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle="rgba(0,0,0,opacity)";
+    } while (opacity < 1.1);
 }
