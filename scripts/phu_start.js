ea24653a3750ea45d5653175e5a6c5bf3d350de8
NY <62198538+NYzyanai@users.noreply.github.com>
2021-02-07 22:45:44 +0900

    yeah

--- scripts/phu_start.js
+++ scripts/phu_start.js
@@ -51,8 +51,95 @@ var movie_imgs_path=[
     'src/movie/r_phu22.png',
     'src/movie/r_phu23.png',
     'src/movie/r_phu24.png',
-    'src/movie/r_phu25.png'
+    'src/movie/r_phu25.png',
+    'src/movie/r_phu26.png',
+    'src/movie/r_phu27.png',
+    'src/movie/r_phu28.png',
+    'src/movie/r_phu29.png',
+    'src/movie/r_phu30.png',
+    'src/movie/r_phu31.png',
+    'src/movie/r_phu32.png',
+    'src/movie/r_phu33.png',
+    'src/movie/r_phu34.png',
+    'src/movie/r_phu35.png',
+    'src/movie/r_phu36.png',
+    'src/movie/r_phu37.png',
+    'src/movie/r_phu38.png',
+    'src/movie/w_line1.png',
+    'src/movie/w_line2.png',
+    'src/movie/w_line3.png',
+    'src/movie/w_line4.png',
+    'src/movie/w_line5.png',
+    'src/movie/w_line6.png',
+    'src/movie/w_line7.png',
+    'src/movie/w_line8.png',
+    'src/movie/w_line9.png',
+    'src/movie/w_line10.png',
+    'src/movie/f_1.png',
+    'src/movie/f_2.png',
+    'src/movie/f_3.png',
+    'src/movie/f_4.png',
+    'src/movie/f_5.png',
+    'src/movie/f_6.png',
+    'src/movie/f_7.png',
+    'src/movie/f_8.png',
+    'src/movie/f_9.png',
+    'src/movie/f_11.png',
+    'src/movie/f_12.png',
+    'src/movie/f_13.png',
+    'src/movie/f_14.png',
+    'src/movie/f_15.png',
+    'src/movie/f_16.png',
+    'src/movie/f_17.png',
+    'src/movie/f_18.png',
+    'src/movie/f_19.png',
+    'src/movie/f_20.png',
+    'src/movie/f_21.png',
+    'src/movie/f_22.png',
+    'src/movie/f_23.png',
+    'src/movie/f_24.png',
+    'src/movie/f_25.png',
+    'src/movie/f_26.png',
+    'src/movie/f_27.png',
+    'src/movie/f_28.png',
+    'src/movie/f_29.png',
+    'src/movie/f_30.png',
+    'src/movie/f_31.png',
+    'src/movie/f_32.png',
+    'src/movie/f_33.png',
+    'src/movie/f_34.png',
+    'src/movie/f_35.png',
+    'src/movie/f_36.png',
+    'src/movie/f_37.png',
+    'src/movie/f_38.png',
+    'src/movie/f_39.png',
+    'src/movie/f_40.png',
+    'src/movie/f_41.png',
+    'src/movie/f_42.png',
+    'src/movie/f_43.png',
+    'src/movie/f_44.png',
+    'src/movie/f_45.png',
+    'src/movie/f_46.png',
+    'src/movie/f_47.png',
+    'src/movie/f_48.png',
+    'src/movie/f_49.png',
+    'src/movie/f_50.png',
+    'src/movie/f_51.png',
+    'src/movie/f_52.png',
+    'src/movie/f_53.png',
+    'src/movie/f_54.png',
+    'src/movie/f_55.png',
+    'src/movie/f_56.png',
+    'src/movie/in_1.png',
+    'src/movie/in_2.png',
+    'src/movie/in_3.png',
+    'src/movie/in_4.png',
+    'src/movie/in_5.png',
+    'src/movie/in_6.png',
+    'src/movie/in_7.png',
+    'src/movie/in_8.png'
 ]
+
 
 
 var movie_imgs=[];
@@ -108,16 +195,22 @@ function movie_draw_image(num){
 var round_movie_loop_count
 var sum_movie_loop_count
 sum_movie_loop_count=0;
+var movie_escape_loop_flag=0;
+
 
 function movie_loop(){
     ctx.drawImage
+    if(between_mil>=0){
     movie_loop_count=movie_loop_count+0.25+(between_mil/1000　);
+    }
 
     console.log(sum_movie_loop_count);
     //いま＋０.３にしてる奴を秒基準にして、端末ごとの歳を無くす
     if(movie_loop_count>=12){
-        movie_loop_count=0.25;
-        //描画に遅れる時間分足す
+        if(movie_escape_loop_flag==0){
+            movie_loop_count=0.25;
+        }
+            //描画に遅れる時間分足す
         sum_movie_loop_count=sum_movie_loop_count+1;
     }
     if(round_movie_loop_count==Math.floor(movie_loop_count)){
@@ -131,6 +224,10 @@ function movie_loop(){
             ctx.drawImage(movie_imgs[round_movie_loop_count],0,0,canvas.width,canvas.height);    
         }else{
             ctx.drawImage(movie_imgs[round_movie_loop_count+11],0,0,canvas.width,canvas.height);  
+            movie_escape_loop_flag=1;
+            if(movie_round_loop_count+11>movie_imgs_path.length){
+                round_movie_loop_count=movie_imgs_path.length-11;
+            }
         }   
         }
 }
