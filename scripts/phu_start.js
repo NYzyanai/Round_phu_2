c86ea8a8da0f74bfcefe362c045a222c8334e69f
NY <62198538+NYzyanai@users.noreply.github.com>
2021-03-20 18:36:58 +0900

    characterq
    yeah

--- scripts/phu_start.js
+++ scripts/phu_start.js
@@ -4,11 +4,16 @@ var phu_stage_flag_before
 phu_stage_flag_before=0;
 
 
-var phu_imgs=[];
-var phu_imgs_path=[];
-var phu_imgCount=0;
+var phu_imgs_background_array=[];
+var phu_imgs_background_namepath=[];
 
-phu_imgs_path=[
+var phu_imgs_character_array=[];
+var phu_imgs_character_namepath=[];
+
+var phu_imgs_background_count=0;
+var phu_imgs_character_count=0;
+
+phu_imgs_background_namepath=[
     'src/stage1_obj/mount_1.png',
     'src/stage1_obj/tree_1.png',
     'src/stage1_obj/tree_2.png',
@@ -17,57 +22,99 @@ phu_imgs_path=[
     'src/stage1_obj/tree_5.png',
     'src/stage1_obj/tree_6.png',
     'src/stage1_obj/tree_7.png',
-    'src/character/stand_phu.png',
-    'src/character/walk_yue_1.png'
+]
+phu_imgs_character_namepath=[
+    'src/character/stand_phu.png'
+    //,
+    //'src/character/walk_yue_1.png'
 ]
 
 
 
-function phu_imageDraw(){
 
+function phu_imgs_background_start(){
     //console.log("movie_imageDrawが呼ばれた")
     canvas = document.getElementById('canvas');  
     ctx=canvas.getContext('2d');
     ctx.imageSmoothingEnabled=false;
 
-    if(phu_loaded_flag==1){
-        phu_draw_image();
-    }else if(phu_loaded_flag==0){
-            phu_load_image();
+    if(phu_imgs_background_loaded_flag==1){
+        phu_imgs_background_draw();
+    }else if(phu_imgs_background_loaded_flag==0){
+        phu_imgs_background_load();
     }
 
 } 
 
+function phu_imgs_character_start(){
+    canvas=document.getElementById('canvas');
+    ctx=canvas.getContext('2d');
+    ctx.imageSmoothingEnabled=false;
 
-var phu_loaded_flag=0;
+    if(phu_imgs_character_loaded_flag==1){
+        phu_imgs_character_draw();
+    }else if(phu_imgs_character_loaded_flag==0){
+        phu_imgs_character_load();
+    }
 
-function phu_load_image(){
+
+}
+
+
+
+
+var phu_imgs_background_loaded_flag=0;
+var phu_imgs_character_loaded_flag=0;
+
+
+function phu_imgs_character_load(){
+    for(var numc=0;numc<phu_imgs_character_namepath.length;numc++){
+        phu_imgs_character_array[numc]=new Image();
+        phu_imgs_character_array[numc].src=phu_imgs_character_namepath[numc];
+
+        phu_imgs_character_array[numc].onload=function(){
+            phu_imgs_character_count=phu_imgs_character_count+1;
+        }
+    }
+    if(phu_imgs_character_count>=phu_imgs_character_namepath.length){
+        phu_imgs_character_loaded_flag=1;
+    }
+}
+
+function phu_imgs_background_load(){
     ///画像読込後にカウントアップ
-    for(var num=0;num<phu_imgs_path.length;num++){
-    phu_imgs[num]=new Image();
-    phu_imgs[num].src=phu_imgs_path[num];
+    for(var num=0;num<phu_imgs_background_namepath.length;num++){
+        phu_imgs_background_array[num]=new Image();
+        phu_imgs_background_array[num].src=phu_imgs_background_namepath[num];
 
-    phu_imgs[num].onload=function(){
-        phu_imgCount=phu_imgCount+1;
-
+        phu_imgs_background_array[num].onload=function(){
+            phu_imgs_background_count=phu_imgs_background_count+1;
+        }  
     }
-    }
-
-    if(phu_imgCount>=phu_imgs_path.length){
-        phu_loaded_flag=1;
-    }
-
-
-
-}
-
-function phu_draw_image(){
-if(phu_imgCount>=phu_imgs_path.length){
-    for (var j=0;j<phu_imgs.length;j++){
-
-    ctx.drawImage(phu_imgs[j],0,0,canvas.width,canvas.height);
+    if(phu_imgs_background_count>=phu_imgs_background_namepath.length){
+        phu_imgs_background_loaded_flag=1;
     }
 }
+
+
+
+
+
+function phu_imgs_character_draw(){
+        console.log("phu_drawまで呼ばれている")
+        for (var k=0;k<phu_imgs_character_namepath.length;k++){
+            ctx.drawImage(phu_imgs_character_array[k],canvas.width*250/320,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+            console.log("kの変数の中身" + k)
+        }
+}
+
+function phu_imgs_background_draw(){
+    if(phu_imgs_background_count>=phu_imgs_background_namepath.length){
+        for (var j=0;j<phu_imgs_background_namepath.length;j++){
+            ctx.drawImage(phu_imgs_background_array[j],0,0,canvas.width,canvas.height);
+        }
+
+    }
 }
 
 var phu_stage_1_started_flag 
@@ -87,7 +134,8 @@ function phu_stage_1(){
     }
 
     if(phu_stage_1_started_flag==1){
-        phu_imageDraw();
+        phu_imgs_background_start();
+        phu_imgs_character_start()
     }
 
 }
