f7b2d56d15989a979fa5aa611cf347f6627214a5
NY <62198538+NYzyanai@users.noreply.github.com>
2021-03-21 15:30:44 +0900

    Update phu_start.js

--- scripts/phu_start.js
+++ scripts/phu_start.js
@@ -135,10 +135,13 @@ var phu_stand_imgs_height
 var phu_stand_imgs_width
 
 
-phu_stand_coord_height=canvas.height*100/180;
+phu_stand_coord_height=canvas.height*105/180;
+phu_stand_coord_width_a=canvas.width*255/320;
+phu_stand_coord_width_b=canvas.width*260/320;
 phu_stand_coord_width=canvas.width*250/320;
 phu_stand_imgs_height=canvas.height*80/180;
 phu_stand_imgs_width=canvas.width*50/320;
+
 
 console.log(phu_walk_flag +"歩くフラグ");
 
@@ -147,46 +150,54 @@ console.log(phu_walk_flag +"歩くフラグ");
         }else if(phu_walk_flag==1){
             if(walk_phu_count<7){
                 phu_walk_num=0;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1
             }else if(walk_phu_count<14){
                 phu_walk_num=1;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1
             }else if(walk_phu_count<21){
                 phu_walk_num=2;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width_a,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1 
             }else if(walk_phu_count<28){
                 phu_walk_num=3;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width_a,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1 
             
             }else if(walk_phu_count<35){
                 phu_walk_num=4;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width_b,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1 
             }else if(walk_phu_count<42){
                 phu_walk_num=5;
-                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width,canvas.height*100/180,canvas.width*50/320,canvas.height*80/180);
+                ctx.drawImage(phu_imgs_character_array[phu_walk_num],phu_stand_coord_width_a,phu_stand_coord_height,canvas.width*50/320,canvas.height*80/180);
                 walk_phu_count=walk_phu_count+1 
             }else{
                 walk_phu_count=0;
                 phu_walk_flag=0;
                 phu_walk_num=0;
+                ctx.drawImage(phu_imgs_character_array[0],phu_stand_coord_width,phu_stand_coord_height,phu_stand_imgs_width,phu_stand_imgs_height);  
             }
         }
 }
 
+var phu_background_coord_width
+var phu_background_coord_height
+phu_background_coord_width=0;
+phu_background_coord_height=0;
+
+
 function phu_imgs_background_draw(){
     if(phu_imgs_background_count>=phu_imgs_background_namepath.length){
         if(phu_walk_flag==1){
+            phu_background_coord_height=phu_background_coord_height+phu_walk_num*2/3
             for (var j=0;j<phu_imgs_background_namepath.length;j++){
-                ctx.drawImage(phu_imgs_background_array[j],0+phu_walk_num*5,0,canvas.width,canvas.height);
+                ctx.drawImage(phu_imgs_background_array[j],phu_background_coord_height,0,canvas.width,canvas.height);
             }
         }else{
             for (var j=0;j<phu_imgs_background_namepath.length;j++){
-                ctx.drawImage(phu_imgs_background_array[j],0,0,canvas.width,canvas.height);
+                ctx.drawImage(phu_imgs_background_array[j],phu_background_coord_height,0,canvas.width,canvas.height);
             } 
         }
 
