607789ffce647e971a1da2aa87cd33fa37552305
NY <62198538+NYzyanai@users.noreply.github.com>
2021-03-21 17:35:41 +0900

    Update touchevent.js

--- scripts/touchevent.js
+++ scripts/touchevent.js
@@ -31,6 +31,33 @@ function setup_touchevent(){
         touchY=e.offsetY*rate_screen_h;
     },false);
 
+    canvas.addEventListener('click',function(e){
+
+        
+        console.log(e.type);
+        console.log("クリックした座標X："+e.offsetX*rate_screen_w);
+        /*console.log("offsetXは？" +e.offsetX);
+        console.log("rate_screem_wは？"+rate_screen_w);*/
+
+        console.log("クリックした座標Y："+e.offsetY*rate_screen_h);
+        /*console.log("offsetYは？" +e.offsetY);
+        console.log("rate_screen_hは？" + rate_screen_h)*/
+
+        /*var yue =new Image();
+        yue.src= "src/yue/stand_yue.png";
+    
+        yue.onload = function(){
+            sizing();
+            /*ctx.imageSmoothingEnabled=false;
+            ctx.drawImage(yue,0,0,yue.width,yue.height);
+            ctx.drawImage(splash_img,0,0,canvas.width,canvas.height);
+        
+        }*/
+        touchX=e.offsetX*rate_screen_w;
+        touchY=e.offsetY*rate_screen_h;
+    },false);
+
+
     canvas.addEventListener('touch',function(e){
 
         
