d2b6d76537e6ba71dfcc5444dc444afe74e460ec
NY <62198538+NYzyanai@users.noreply.github.com>
2021-05-05 21:00:33 +0900

    Update phu_start.js

--- scripts/phu_start.js
+++ scripts/phu_start.js
@@ -504,8 +504,12 @@ function judge_draw(){
 
         //一回の歩行で25マス、歩行時には30回呼ばれるので、25/29(30-1にしないと１ます　遅くなる)
 
-        all_step_count=all_step_count+25/24+speedup;
 
+        if(through_flag==1 && through_count<100){
+            
+        }else{
+            all_step_count=all_step_count+25/24+speedup;
+        }
         
         if(draw_phu_count==4 && walk_time_img_count<5){
             walk_time_img_count=walk_time_img_count+1;
@@ -515,8 +519,13 @@ function judge_draw(){
             draw_phu_count=0;
             walk_time_img_count=0;
             walk_start_flag=0;
-            call_walk_count=call_walk_count+1;
-            all_step_count=call_walk_count*25;
+            
+            if(through_flag==1 && through_count<100){
+            
+            }else{
+                all_step_count=call_walk_count*25;
+                call_walk_count=call_walk_count+1;
+            }
         }
         
         
@@ -717,8 +726,15 @@ function judge_draw(){
         //Phuの歩行関数
         character5=phu_imgs_phu_array[walk_time_img_count];
         //character5_1=phu_imgs_yue_array[walk_time_img_count];
-    
+        
         character5_c_w=225+((walk_time_img_count)*14/5);
+        if(through_flag==1){
+            if(walk_time_img_count>0 && through_count<100){
+            through_count=through_count+1;
+            }
+            character5_c_w=225-through_count+((walk_time_img_count)*14/5);
+        }
+        
         //14マスを5枚で移動する
         character5_c_h=105;
 
@@ -953,10 +969,10 @@ function judge_draw(){
             if(ghost_end_text_flag==0){
                 drawtext(11,22,"＊＊＊＊ : ",2);
 
-                character5_c_w=225+((walk_time_img_count)*14/5)-30;
+                through_flag=1;
                 
                 stop_walk_flag=1;
-                if(gyou_buf>1){
+                if(gyou_buf>2){
                     stop_walk_flag=0;
                 }
             }else{
@@ -971,6 +987,8 @@ function judge_draw(){
 var lay
 lay=600;
 
+var through_flag=0;
+var through_count=0;
 var gyou_count=0;
 var text_time=0;
 
