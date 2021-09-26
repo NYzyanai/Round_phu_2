7b0811889dc7601b47c81a9ee34e9e5c8769a86e
NY <62198538+NYzyanai@users.noreply.github.com>
2021-09-26 13:38:37 +0900

    設定を選んだら音声

--- scripts/music.js
+++ scripts/music.js
@@ -7,12 +7,23 @@ function play_music() {
     audio.play();
 }
 
-function play_text(){
+function play_text() {
     var text_audio = document.querySelector(".text_audio");
-//console.log(document.querySelector('.text_audio'));
-//console.log(text_audio);
-//console.log(text_audio.volume); // 1
-text_audio.currentTime = 0;
-text_audio.volume = 1;
-text_audio.play();
+    //console.log(document.querySelector('.text_audio'));
+    //console.log(text_audio);
+    //console.log(text_audio.volume); // 1
+    text_audio.currentTime = 0;
+    text_audio.volume = 1;
+    text_audio.play();
+}
+
+
+function settings_select() {
+    var settings_select = document.querySelector(".settings_select");
+    //console.log(document.querySelector('.text_audio'));
+    //console.log(text_audio);
+    //console.log(text_audio.volume); // 1
+    settings_select.currentTime = 0;
+    settings_select.volume = 1;
+    settings_select.play();
 }
